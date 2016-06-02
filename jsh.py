460d535b784686de549e370fa5278ff8bb8bf06c
raveneus <raveneus.cwillguy@gmail.com>
2016-06-02 12:16:49 -0500

    Update jsh.py

--- jsh.py
+++ jsh.py
@@ -1,13 +1,19 @@
-import os, sys, subprocess, cmd
+import os, sys, subprocess, cmd, ConfigParser
 vars = {"PATH":"/usr/bin:/bin", "MODULES":""}
 
 def getModules(vars):
-    pass
+    parser = ConfigParser.ConfigParser()
+    parser.open(".jshc")
+    vars["MODULES"] = parser.get("jsh", "MODULES")
+
 
 class jsh(cmd.Cmd):
     def __init__(self, vars):
         cmd.Cmd.__init__(self)
         self.vars = vars
+    def help_help(self):
+        print "Usage: help [cmd]"
+        print "help: show commands or get help on a command"
 
 def main(vars):
     getModules(vars)
