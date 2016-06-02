56882a1ba60a5643818e9f0f84c9fd75d67d49de
raveneus <raveneus.cwillguy@gmail.com>
2016-06-02 10:51:25 -0500

    Update jsh.py

--- jsh.py
+++ jsh.py
@@ -1,7 +1,18 @@
-import os, sys
+import os, sys, subprocess, cmd
+vars = {"PATH":"/usr/bin:/bin", "MODULES":""}
 
-def main():
+def getModules(vars):
     pass
 
+class jsh(cmd.Cmd):
+    def __init__(self, vars):
+        cmd.Cmd.__init__(self)
+        self.vars = vars
+
+def main(vars):
+    getModules(vars)
+    m = jsh(vars)
+    m.cmdloop()
+
 if __name__ == '__main__':
-    main()
+    main(vars)
